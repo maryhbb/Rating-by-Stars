f56869f42e4f9a27433419cb0914a3e33f032c06
Mary <mary_hbb@yahoo.com.com>
2021-09-23 19:20:20 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Event } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inputoutput';
+  ratings: number[] = [3.5, 4, 1.5, 2, 4.5, 2.5];
+
+
+  public onStarClicked(rating: number){
+    console.log(`The Rating ${rating} is clicked`)
+
+  }
+
+
 }
---
+++ src/app/star/star.component.ts
@@ -0,0 +1,23 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css']
+})
+export class StarComponent implements OnInit {
+
+@Input() rating: number = 3.5;
+@Output() starClicked = new EventEmitter<number>();
+width: number = 95;
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    this.width = this.rating * 95 / 5;
+  }
+ public onClick(){
+   this.starClicked.emit(this.rating);
+ }
+}
