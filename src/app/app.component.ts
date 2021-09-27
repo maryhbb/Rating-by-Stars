593a0ce4723632d93da58f7deba6cd8e2ace5503
Mary <mary_hbb@yahoo.com>
2021-09-28 00:04:20 +0200

    update review comments

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'input-output';
-  ratings: number[] = [3.5, 4, 1.5, 2, 4.5, 2.5];
+  public ratings: number[] = [3.5, 4, 1.5, 2, 4.5, 2.5];
 
   public onStarClicked(rating: number) {
-    console.log(`The Rating ${rating} is clicked`)
-
+    console.log(`The Rating ${rating} is clicked`);
   }
 }
